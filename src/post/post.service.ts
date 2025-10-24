447acb673e52c047213d468dab49fbff3490213d
Vittor <vittorfranceschi04@gmail.com>
2025-10-24 06:17:58 -0300

    Dto posts

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,26 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Post } from './entities/post.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
 import { CreatePostDto } from './dto/create-post.dto';
-import { UpdatePostDto } from './dto/update-post.dto';
 
 @Injectable()
 export class PostService {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
-  }
+  constructor(
+    @InjectRepository(Post)
+    private readonly postRepository: Repository<Post>,
+  ) {}
 
-  findAll() {
-    return `This action returns all post`;
-  }
+  async create(author: User, dto: CreatePostDto) {
+    const newPost = this.postRepository.create({
+      title: dto.title,
+      excerpt: dto.excerpt,
+      content: dto.content,
+      author,
+    });
 
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
-  }
-
-  update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} post`;
+    return await this.postRepository.save(newPost);
   }
 }
