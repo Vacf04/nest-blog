27ec309331373cf027c64f176af6f449a9d95629
Vittor <vittorfranceschi04@gmail.com>
2025-10-27 11:57:35 -0300

    Posts routes

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,8 +1,10 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   Param,
+  Patch,
   Post,
   Req,
   UseGuards,
@@ -12,6 +14,7 @@ import { AuthGuard } from '@nestjs/passport';
 import type { AuthenticatedRequest } from 'src/auth/types/authenticated-requests';
 import { CreatePostDto } from './dto/create-post.dto';
 import { PostResponseDto } from './dto/post-response.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
 
 @Controller('post')
 export class PostController {
@@ -30,6 +33,16 @@ export class PostController {
     return posts.map((post) => new PostResponseDto(post));
   }
 
+  @UseGuards(AuthGuard('jwt'))
+  @Get('me/:slug')
+  async readOnePostOwned(
+    @Req() req: AuthenticatedRequest,
+    @Param('slug') slug: string,
+  ) {
+    const post = await this.postService.readOnePostOwned(req.user.id, slug);
+    return new PostResponseDto(post);
+  }
+
   @Get(':slug')
   async readOnePostPublic(@Param('slug') slug: string) {
     const post = await this.postService.readOnePostPublic(slug);
@@ -40,6 +53,24 @@ export class PostController {
   @Post('me')
   async create(@Req() req: AuthenticatedRequest, @Body() dto: CreatePostDto) {
     const post = await this.postService.create(req.user, dto);
+    return new PostResponseDto(post);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Patch('me/:slug')
+  async update(
+    @Req() req: AuthenticatedRequest,
+    @Body() dto: UpdatePostDto,
+    @Param('slug') slug: string,
+  ) {
+    const post = await this.postService.update(slug, dto, req.user);
+    return new PostResponseDto(post);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Delete('me/:slug')
+  async remove(@Req() req: AuthenticatedRequest, @Param('slug') slug: string) {
+    const post = await this.postService.remove(slug, req.user);
     return new PostResponseDto(post);
   }
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -10,6 +10,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
 import { CreatePostDto } from './dto/create-post.dto';
 import { createSlugFromText } from './utils/create-slug-from-text';
+import { UpdatePostDto } from './dto/update-post.dto';
 
 @Injectable()
 export class PostService {
@@ -33,13 +34,36 @@ export class PostService {
       .save(post)
       .catch((err: unknown) => {
         if (err instanceof Error) {
-          this.logger.error('Erro ao criar post', err.stack);
+          this.logger.error('Error creating post', err.stack);
         }
 
-        throw new BadRequestException('Erro ao criar o post');
+        throw new BadRequestException('Error creating post');
       });
 
     return created;
+  }
+
+  async update(slug: string, dto: UpdatePostDto, user: User) {
+    if (Object.keys(dto).length === 0) {
+      throw new BadRequestException('Dados não enviados');
+    }
+
+    const post = await this.postRepository.findOne({
+      where: { slug, author: { id: user.id } },
+      relations: ['author'],
+    });
+
+    if (!post) {
+      throw new NotFoundException('Post not found.');
+    }
+
+    post.title = dto.title ?? post.title;
+    post.content = dto.content ?? post.content;
+    post.excerpt = dto.excerpt ?? post.excerpt;
+    post.coverImageUrl = dto.coverImageUrl ?? post.coverImageUrl;
+    post.published = dto.published ?? post.published;
+
+    return this.postRepository.save(post);
   }
 
   async readAllPostPublic() {
@@ -96,5 +120,20 @@ export class PostService {
     });
 
     return posts;
+  }
+
+  async remove(slug: string, user: User) {
+    const post = await this.postRepository.findOne({
+      where: { slug, author: { id: user.id } },
+      relations: ['author'],
+    });
+
+    if (!post) {
+      throw new NotFoundException('Post not found.');
+    }
+
+    await this.postRepository.delete(post);
+
+    return post;
   }
 }
